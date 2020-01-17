d38e555a126460caf7c20a5e6644db223d93421f
Anastasia Konopatova <32372877+konopatova@users.noreply.github.com>
2020-01-17 09:54:44 +0300

    footer bottom and social icons sprite

--- build/js/modules/fixed-header.js
+++ build/js/modules/fixed-header.js
@@ -1,87 +1,59 @@
 'use strict';
 
 (function () {
-  // function headerScroll() {
-  //   if (document.body.scrollTop > 350) {
-  //     header.classList.add('header--active');
-  //   }
-  // }
-  // window.addEventListener('scroll', headerScroll);
 
   window.addEventListener('scroll', function () {
     var scrolled = window.scrollY;
     var header = document.querySelector(".header");
-    scrolled > 30 ?
-      header.classList.add('header--active') : header.classList.remove('header--active')
+    scrolled > 20 ? header.classList.add('header--active') : header.classList.remove('header--active')
   })
 
 })();
 
 
-// (function () {
-
-//   var burgerItem = document.querySelector('.burger');
-//   var menu = document.querySelector('.header__nav');
-//   var menuCloseItem = document.querySelector('.header__nav-close');
-//   var menuLinks = document.querySelectorAll('.header__link');
-//   var disableBodyScroll = bodyScrollLock.disableBodyScroll;
-//   var enableBodyScroll = bodyScrollLock.enableBodyScroll;
-
-//   burgerItem.addEventListener('click', function (open) {
-//     menu.classList.add('header__nav--active');
-//     disableBodyScroll();
-//   });
-//   menuCloseItem.addEventListener('click', function (close) {
-//     menu.classList.remove('header__nav--active');
-//     enableBodyScroll();
-//   });
-
-//   if (window.innerWidth <= 767) {
-//     for (let i = 0; i < menuLinks.length; i += 1) {
-//       menuLinks[i].addEventListener('click', function () {
-//         menu.classList.remove('header__nav--active');
-//       });
-//     }
-//   }
-
-// })();
-
-// Scroll to anchors class .js-scroll minus header height
 (function () {
 
-  const smoothScroll = function (targetEl, duration) {
-    const headerElHeight = document.querySelector('.header').clientHeight;
-    let target = document.querySelector(targetEl);
-    let targetPosition = target.getBoundingClientRect().top - headerElHeight;
-    let startPosition = window.pageYOffset;
-    let startTime = null;
+  var burgerButton = document.querySelector('.burger-button__open');
+  var closeButton = document.querySelector('.burger-button__close');
+  var menu = document.querySelector('.dropdown-menu');
+  var menuLinks = document.querySelectorAll('.menu__item-link');
+  var isOpened = menu.classList.contains('dropdown-menu--opened');
+  var isClosed = menu.classList.contains('dropdown-menu--closed');
+  var disableBodyScroll = bodyScrollLock.disableBodyScroll;
+  var enableBodyScroll = bodyScrollLock.enableBodyScroll;
+  var screenWidth;
 
-    const ease = function (t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return c / 2 * t * t + b;
-      t--;
-      return -c / 2 * (t * (t - 2) - 1) + b;
-    };
+  if (!burgerButton) {
+    return;
+  }
 
-    const animation = function (currentTime) {
-      if (startTime === null) startTime = currentTime;
-      const timeElapsed = currentTime - startTime;
-      const run = ease(timeElapsed, startPosition, targetPosition, duration);
-      window.scrollTo(0, run);
-      if (timeElapsed < duration) requestAnimationFrame(animation);
-    };
-    requestAnimationFrame(animation);
+  burgerButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    menu.classList.remove('dropdown-menu--closed');
+    menu.classList.add('dropdown-menu--opened');
+    burgerButton.classList.add('hidden');
+    closeButton.classList.remove('hidden');
+    disableBodyScroll();
+  })
 
-  };
 
-  const scrollTo = function () {
-    const links = document.querySelectorAll('.js-scroll');
-    links.forEach(each => {
-      each.addEventListener('click', function () {
-        const currentTarget = this.getAttribute('href');
-        smoothScroll(currentTarget, 1000);
-      });
+  closeButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    menu.classList.remove('dropdown-menu--opened');
+    menu.classList.add('dropdown-menu--closed');
+    burgerButton.classList.remove('hidden');
+    closeButton.classList.add('hidden');
+    enableBodyScroll();
+  })
+
+  for (let i = 0; i < menuLinks.length; i += 1) {
+    menuLinks[i].addEventListener('click', function () {
+      menu.classList.remove('dropdown-menu--opened');
+      menu.classList.add('dropdown-menu--closed');
+      burgerButton.classList.remove('hidden');
+      closeButton.classList.add('hidden');
+      enableBodyScroll();
     });
-  };
-  scrollTo();
-}());
+  }
+
+})();
